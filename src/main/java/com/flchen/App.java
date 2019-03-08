db15e584f08a7694813befaebe275a8ee4fce954
mrCalmdow <634233816@qq.com>
2019-03-08 14:20:37 +0800

    initializ project

---
+++ src/main/java/com/flchen/App.java
@@ -0,0 +1,13 @@
+package com.flchen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
